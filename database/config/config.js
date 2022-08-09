ed41901c350c01871874359a5935b7249968db87
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-08-09 12:05:00 -0300

    sql connection

--- database/config/config.js
+++ database/config/config.js
@@ -14,10 +14,10 @@ module.exports={
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bf19f99df793b3",
+    "password": "393525e8",
+    "database": "heroku_80ac6426bbac242",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   }
 }
